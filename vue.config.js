b746827c3b53434997c8544dc292e59fdc55fbe4
Pau Almirall <pau.almirall@lidl.es>
2019-06-19 17:02:05 +0200

    comment externals

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,16 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
       ? 'https://paualmirallmicro1.netlify.com/'
       : '/',
-    chainWebpack: config => {
-      config.externals({
-        'lodash': {
-          commonjs: 'lodash',
-          commonjs2: 'lodash',
-          amd: 'lodash',
-          root: '_',
-        },
-        vue: "Vue",
-      })
-    }
+    // chainWebpack: config => {
+    //   config.externals({
+    //     'lodash': {
+    //       commonjs: 'lodash',
+    //       commonjs2: 'lodash',
+    //       amd: 'lodash',
+    //       root: '_',
+    //     },
+    //     vue: "Vue",
+    //     "core-js": "core.js",
+    //   })
+    // }
   }
